2dd8fcc11f749ed98b1140193da96a4fd430a4d2
Harrison <hchap1@hotmail.com>
2025-02-25 11:20:12 +1000

    Added start of multi worker system

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,6 +1,8 @@
 use std::thread::{JoinHandle, spawn, sleep};
 use std::sync::{Arc, Mutex};
+use std::process::Command;
 use std::time::Duration;
+use std::path::PathBuf;
 
 use crate::music::Song;
 
@@ -23,37 +25,60 @@ struct Task {
 
 struct Downloader {
     tasks: Vec<Task>,
-    workers: Vec<JoinHandle<()>>
+    workers: Vec<Worker>,
+    directory: PathBuf
 }
 
 struct Worker {
     running: AM<bool>,
     todo: AMV<Task>,
-    handle: JoinHandle<()>
+    handle: Option<JoinHandle<()>>,
+    directory: PathBuf
 }
 
-/// Spawn a worker thread, querying for tasks constantly
-fn worker_thread(todo_ref: AMV<Task>, running_ref: AM<bool>) {
+/// Spawn a worker thread to complete a task
+fn worker_thread(todo_ref: AMV<Task>, running_ref: AM<bool>, directory: PathBuf) {
     let period: Duration = Duration::from_secs(1);
     loop {
-        sleep(period);
         // Check whether to exit then implicitly release mutex
         {
             let running = running_ref.lock().unwrap();
             if !*running {
                 return;
             }
+        } {
+            let mut todo = todo_ref.lock().unwrap();
+            if todo.len() == 0 {
+
+                // If there is nothing left to do, terminate the thread.
+                let mut running = running_ref.lock().unwrap();
+                *running = false;
+
+            } else {
+
+                // Else, complete the task, blocking until it finishes then removing it
+                todo.remove(0).execute(&directory);
+            }
         }
+        sleep(period);
     }
 }
 
 impl Worker {
-    pub fn new() -> Self {
+    pub fn new(directory: PathBuf) -> Self {
         let todo: AMV<Task> = sync::<Vec<Task>>(Vec::<Task>::new());
         let running: AM<bool> = sync::<bool>(true);
-        let todo_ref = Arc::clone(&todo);
-        let running_ref = Arc::clone(&running);
-        let handle: JoinHandle<()> = spawn(move || worker_thread(todo_ref, running_ref));
+        Self { running, todo, handle: None, directory }
+    }
+
+    fn queue(&mut self, task: Task) {
+        let mut running = self.running.lock().unwrap();
+
+        // If the thread is already running, simply add this to the queue
+        // Else, make a thread and do the same
+        if running {
+            
+        }
     }
 }
 
@@ -61,11 +86,30 @@ impl Task {
     pub fn download(song: Song) -> Self {
         Self { target: song, action: Action::Download }
     }
+
+    fn execute(&mut self, directory: &PathBuf) {
+        match self.action {
+
+            Action::Download => {
+                // Create a command that runs an instance of yt-dlp
+
+                let handle = Command::new("yt-dlp").arg("-f").arg("bestaudio").arg("--extract-audio").arg("--audio-format").arg("mp3").arg("-o").arg(format!(
+                    "{}/{}.mp3", directory.to_string_lossy().to_string(), file_id
+                )).arg(&self.url).stdout(std::process::Stdio::null()).stderr(std::process::Stdio::null()).spawn();
+            }
+
+            Action::Remove => {
+
+            }
+        }
+    }
 }
 
 impl Downloader {
-    pub fn new() -> Self {
-        Self { tasks: Vec::new(), workers: Vec::new() }
+    pub fn new(directory: PathBuf) -> Self {
+        let mut workers: Vec<Worker> = Vec::new();
+        for _ in 0..4 { workers.push(Worker::new(directory.clone())); }
+        Self { tasks: Vec::new(), workers, directory }
     }
 
     pub fn async_execute(&mut self, task: Task) {
