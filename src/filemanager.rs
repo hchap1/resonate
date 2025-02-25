3f458e05ab3bfd19aa4a2f84cace9a6c5727749d
Harrison <hchap1@hotmail.com>
2025-02-25 23:15:47 +1000

    Added caching paradigm - any searching will go through both youtube music AND cached songs, playlists are just lists of song ID

--- src/filemanager.rs
+++ src/filemanager.rs
@@ -1,5 +1,11 @@
-use directories::ProjectDirs;
 use std::{fs::create_dir_all, path::PathBuf};
+use std::collections::HashMap;
+use directories::ProjectDirs;
+use std::fs::read_to_string;
+use std::io::Write;
+use std::fs::File;
+
+use crate::music::Song;
 
 /// Creates and then returns the path to a suitable location for application data to be stored.
 /// If the path already exists, just return the path.
@@ -12,4 +18,83 @@ pub fn get_application_directory() -> Result<PathBuf, ()> {
     let path = project_dir.data_dir().to_path_buf();
     let _ = create_dir_all(&path);
     Ok(path)
+}
+
+// This is a record of all touched songs, any time a search is made the results are added
+pub struct SongCache {
+    songs: Vec<Song>,
+    metahash: HashMap<String, usize>, // hashmap relating the ID the idx of the song in the cache
+    file: PathBuf
+}
+
+impl SongCache {
+
+    fn readlines(&self) -> Vec<String> {
+        match read_to_string(&self.file) {
+            Ok(contents) => contents.lines().map(|x| x.to_string()).collect::<Vec<String>>(),
+            Err(_) => Vec::<String>::new()
+        }
+    }
+
+    fn rebuild_hashmap(&mut self) {
+        self.metahash.clear();
+        self.songs.iter().enumerate().for_each(|song| {
+            self.metahash.insert(song.1.id.clone(), song.0);
+        });
+    }
+
+    fn reload_cache(&mut self) {
+        self.metahash.clear();
+        self.songs = self.readlines().into_iter().enumerate().map(|line| {
+            let song = Song::deserialise(line.1);
+            self.metahash.insert(song.id.clone(), line.0);
+            song
+        }).collect::<Vec<Song>>();
+    }
+
+    fn rewrite_file(&self) {
+        let mut file: File = File::create(&self.file).unwrap();
+        self.songs.iter().for_each(|song| writeln!(file, "{}", song.serialise()).unwrap());
+    }
+
+    /// Load the SongCache from file. If no such file exists, it will be created
+    pub fn load(directory: PathBuf) -> Self {
+        let file = directory.join(PathBuf::from("songcache.txt"));
+        if !file.exists() {
+            let _ = File::create(&file);
+        }
+
+        let mut song_cache = Self {
+            songs: Vec::<Song>::new(),
+            file,
+            metahash: HashMap::<String, usize>::new()
+        };
+
+        song_cache.reload_cache();
+        song_cache
+    }
+
+    pub fn get_song_data_by_id(&self, id: &String) -> Option<Song> {
+        match self.metahash.get(id) {
+            Some(idx) => Some(self.songs[*idx].clone()),
+            None => None
+        }
+    }
+
+    pub fn add_song(&mut self, song: Song) {
+        self.metahash.insert(song.id.clone(), self.songs.len());
+        self.songs.push(song);
+        self.rewrite_file();
+    }
+
+    pub fn remove_song(&mut self, song: Song) {
+        let idx = match self.metahash.get(&song.id) {
+            Some(idx) => idx.clone(),
+            None => return
+        };
+
+        self.songs.remove(idx);
+        self.rebuild_hashmap();
+        self.rewrite_file();
+    }
 }
--- src/music.rs
+++ src/music.rs
@@ -1,5 +1,8 @@
 use std::path::PathBuf;
 
+const DELIM: char = '͵'; // Unicode 0372, greek lower numeral sign
+
+#[derive(Clone)]
 pub struct Song {
     pub name: String,
     pub artist: String,
@@ -7,8 +10,37 @@ pub struct Song {
     pub file: Option<PathBuf>
 }
 
+pub struct Playlist {
+    pub name: String,
+    pub songs: Vec<Song>
+}
+
 impl Song {
     pub fn new(name: String, artist: String, id: String, file: Option<PathBuf>) -> Self {
         Self { name, artist, id, file }
+    }
+
+    pub fn deserialise(serial: String) -> Self {
+        // Format in cache is:
+        // name|artist|id|file, if file is none then _ else absolute filepath
+        let components = serial.split(DELIM).map(|x| x.to_string()).collect::<Vec<String>>();
+        Self {
+            name: components[0].clone(),
+            artist: components[1].clone(),
+            id: components[2].clone(),
+            file: if &components[3] == "_" { None } else { Some(PathBuf::from(&components[3])) }
+        }
+    }
+
+    pub fn serialise(&self) -> String {
+        format!("{}{DELIM}{}{DELIM}{}{DELIM}{}",
+            self.name,
+            self.artist,
+            self.id,
+            match &self.file {
+                Some(filepath) => filepath.to_string_lossy().to_string(),
+                None => String::from("_")
+            }
+        )
     }
 }
