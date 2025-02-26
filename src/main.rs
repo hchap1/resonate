7a7c521bffd0d493695f4fada7c0f3b1bb4bc9cd
Harrison <hchap1@hotmail.com>
2025-02-27 09:57:41 +1000

    switched to an sqlite database structure

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,22 @@ use downloader::Task;
 
 use crate::music::Song;
 use crate::downloader::Downloader;
+use crate::filemanager::Database;
 use crate::filemanager::get_application_directory;
 
 fn main() {
     let directory: PathBuf = get_application_directory().unwrap();
-    println!("directory: {}", directory.to_string_lossy().to_string());
-    let mut downloader: Downloader = Downloader::new(directory);
-
-
-    let song1: Song = Song::new(
-        String::from("Clocks"),
-        String::from("Coldplay"),
-        String::from("8Xv_Hg8o1fw"),
-        None
-    );
-
-    let song2: Song = Song::new(
+    let database: Database = Database::new(directory);
+    database.add_song_to_cache(&Song::new(
         String::from("Paradise"),
         String::from("Coldplay"),
-        String::from("Q0TEUMPIhk8"),
+        String::from("some_id"),
+        0,
         None
-    );
+    ));
 
-    downloader.async_execute(Task::download(song1));
-    downloader.async_execute(Task::download(song2));
-
-    loop {
-        sleep(Duration::from_millis(300));
-        let workers = downloader.get_busy_worker_count();
-        println!("BUSY WORKERS: {workers}");
-        if workers == 0 { break; }
+    let res = database.search_cached_song(String::from("para"));
+    for r in res {
+        println!("SONG -> {}", r.name);
     }
 }
--- src/music.rs
+++ src/music.rs
@@ -7,6 +7,7 @@ pub struct Song {
     pub name: String,
     pub artist: String,
     pub id: String,
+    pub duration: usize,
     pub file: Option<PathBuf>
 }
 
@@ -16,31 +17,7 @@ pub struct Playlist {
 }
 
 impl Song {
-    pub fn new(name: String, artist: String, id: String, file: Option<PathBuf>) -> Self {
-        Self { name, artist, id, file }
-    }
-
-    pub fn deserialise(serial: String) -> Self {
-        // Format in cache is:
-        // name|artist|id|file, if file is none then _ else absolute filepath
-        let components = serial.split(DELIM).map(|x| x.to_string()).collect::<Vec<String>>();
-        Self {
-            name: components[0].clone(),
-            artist: components[1].clone(),
-            id: components[2].clone(),
-            file: if &components[3] == "_" { None } else { Some(PathBuf::from(&components[3])) }
-        }
-    }
-
-    pub fn serialise(&self) -> String {
-        format!("{}{DELIM}{}{DELIM}{}{DELIM}{}",
-            self.name,
-            self.artist,
-            self.id,
-            match &self.file {
-                Some(filepath) => filepath.to_string_lossy().to_string(),
-                None => String::from("_")
-            }
-        )
+    pub fn new(name: String, artist: String, id: String, duration: usize, file: Option<PathBuf>) -> Self {
+        Self { name, artist, id, duration, file }
     }
 }
