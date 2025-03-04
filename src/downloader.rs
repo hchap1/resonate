20756d6739ed63dd7a1135ad7191175c7c9290f1
Harrison <hchap1@hotmail.com>
2025-03-04 21:51:04 +1000

    removed old multi-worker paradigm

--- src/downloader.rs
+++ src/downloader.rs
@@ -11,250 +11,34 @@ use crate::application::Message;
 use crate::music::Song;
 use crate::utility::{AM, AMV, sync};
 
-enum Action {
-    Download,
-    Remove
+struct DownloadTask {
+    
 }
 
-pub struct Task {
-    target: Song,
-    action: Action
-}
-
-pub struct Downloader {
-    tasks: Vec<Task>,
-    workers: Vec<Worker>,
-    directory: PathBuf
-}
-
-struct Worker {
-    running: AM<bool>,
-    todo: AMV<Task>,
-    handle: Option<JoinHandle<()>>,
-    directory: PathBuf
-}
-
-/// Spawn a worker thread to complete a task
-fn worker_thread(todo_ref: AMV<Task>, running_ref: AM<bool>, directory: PathBuf) {
-    let period: Duration = Duration::from_secs(1);
-    loop {
-        // Check whether to exit then implicitly release mutex
-        {
-            let running = running_ref.lock().unwrap();
-            if !*running {
-                return;
-            }
+impl DownloadTask {
+    pub fn new() -> Self {
+        let task_path = directory.join(PathBuf::from(format!("{}.mp3", self.target.id)));
+        println!("[WORKER] Using task_path {}", task_path.to_string_lossy().to_string());
+        if task_path.exists() {
+            return;
         }
 
-        let task: Option<Task> = {
-            let mut todo = todo_ref.lock().unwrap();
-            if todo.len() == 0 {
+        let mut handle = Command::new("yt-dlp")
+            .arg("-f")
+            .arg("bestaudio")
+            .arg("--extract-audio")
+            .arg("--audio-format")
+            .arg("mp3")
+            .arg("-o")
+            .arg(format!("{}/{}.mp3", directory.to_string_lossy().to_string(), self.target.id))
+            .arg(format!("https://music.youtube.com/watch?v={}", self.target.id))
+            .stdout(std::process::Stdio::null())
+            .stderr(std::process::Stdio::null())
+            .spawn().unwrap();
 
-                // If there is nothing left to do, terminate the thread.
-                let mut running = running_ref.lock().unwrap();
-                *running = false;
-                None
+        let _ = handle.wait();
 
-            } else {
-
-                // Else, complete the task, blocking until it finishes then removing it
-                Some(todo.remove(0))
-            }
-        };
-
-        if let Some(mut task) = task {
-            task.execute(&directory);
-        }
-
-        sleep(period);
-    }
-}
-
-impl Worker {
-    pub fn new(directory: PathBuf) -> Self {
-        let todo: AMV<Task> = sync::<Vec<Task>>(Vec::<Task>::new());
-        let running: AM<bool> = sync::<bool>(false);
-        Self { running, todo, handle: None, directory }
-    }
-
-    fn queue(&mut self, task: Task) {
-        let running = { if *self.running.lock().unwrap() { true } else { false } };
-
-        // If the thread is already running, simply add this to the queue
-        // Else, make a thread and do the same
-        if running {
-
-            let mut todo = self.todo.lock().unwrap();
-            todo.push(task);
-
-        } else {
-
-            let todo_ref = Arc::clone(&self.todo);
-            let running_ref = Arc::clone(&self.running);
-            let directory_clone = self.directory.clone();
-
-            {
-                let mut todo = self.todo.lock().unwrap();
-                todo.push(task);
-
-                let mut running = self.running.lock().unwrap();
-                *running = true;
-            }
-
-            self.handle = Some(spawn(move || worker_thread(todo_ref, running_ref, directory_clone)));
-        }
-    }
-
-    fn queue_until_completion(&mut self, task: Task) -> Vec<Song> {
-        let running = { if *self.running.lock().unwrap() { true } else { false } };
-        let mut songs: Vec<Song> = vec![];
-
-        // If the thread is already running, simply add this to the queue
-        // Else, make a thread and do the same
-        if running {
-
-            let mut todo = self.todo.lock().unwrap();
-            todo.push(task);
-            for item in todo.iter() {
-                let mut song = item.target.clone();
-                song.file = Some(self.directory.join(format!("{}.mp3", song.id)));
-                songs.push(song);
-            }
-
-        } else {
-
-            let todo_ref = Arc::clone(&self.todo);
-            let running_ref = Arc::clone(&self.running);
-            let directory_clone = self.directory.clone();
-
-            {
-                let mut todo = self.todo.lock().unwrap();
-                todo.push(task);
-
-                for item in todo.iter() {
-                    let mut song = item.target.clone();
-                    song.file = Some(self.directory.join(format!("{}.mp3", song.id)));
-                    songs.push(song);
-                }
-
-                let mut running = self.running.lock().unwrap();
-                *running = true;
-            }
-
-            self.handle = Some(spawn(move || worker_thread(todo_ref, running_ref, directory_clone)));
-        }
-        let _ = self.handle.take().unwrap().join();
-        songs
-    }
-
-    fn query_task_length(&self) -> Option<usize> {
-        let running = self.running.lock().unwrap();
-        let todo = self.todo.lock().unwrap();
-        if *running { Some(todo.len()) } else { None }
-    }
-}
-
-impl Task {
-    pub fn download(song: Song) -> Self {
-        Self { target: song, action: Action::Download }
-    }
-
-    fn execute(&mut self, directory: &PathBuf) {
-        match self.action {
-
-            Action::Download => {
-                // Create a command that runs an instance of yt-dlp
-
-                let task_path = directory.join(PathBuf::from(format!("{}.mp3", self.target.id)));
-                println!("[WORKER] Using task_path {}", task_path.to_string_lossy().to_string());
-                if task_path.exists() {
-                    return;
-                }
-
-                let mut handle = Command::new("yt-dlp")
-                    .arg("-f")
-                    .arg("bestaudio")
-                    .arg("--extract-audio")
-                    .arg("--audio-format")
-                    .arg("mp3")
-                    .arg("-o")
-                    .arg(format!("{}/{}.mp3", directory.to_string_lossy().to_string(), self.target.id))
-                    .arg(format!("https://music.youtube.com/watch?v={}", self.target.id))
-                    .stdout(std::process::Stdio::null())
-                    .stderr(std::process::Stdio::null())
-                    .spawn().unwrap();
-
-                let _ = handle.wait();
-
-                println!("[WORKER] finished downloading {}", self.target.name);
-
-            }
-
-            Action::Remove => {
-                
-            }
-        }
-    }
-}
-
-impl Downloader {
-    pub fn new(directory: PathBuf) -> Self {
-        let mut workers: Vec<Worker> = Vec::new();
-        for _ in 0..4 { workers.push(Worker::new(directory.clone())); }
-        Self { tasks: Vec::new(), workers, directory }
-    }
-
-    pub fn async_execute(&mut self, task: Task) {
-        
-        let mut minimum_workload: usize = 0;
-        let mut most_free_worker: usize = 0;
-        
-        for idx in 0..self.workers.len() {
-            match self.workers[idx].query_task_length() {
-                Some(number_of_tasks) => {
-                    if number_of_tasks < minimum_workload {
-                        most_free_worker = idx;
-                        minimum_workload = number_of_tasks;
-                    }
-                }
-                None => {
-                    self.workers[idx].queue(task);
-                    return;
-                }
-            }
-        }
-
-        self.workers[most_free_worker].queue(task);
-    }
-
-    pub async fn wait_for_execution(&mut self, task: Task) -> Message {
-
-        let mut minimum_workload: usize = 0;
-        let mut most_free_worker: usize = 0;
-        
-        for idx in 0..self.workers.len() {
-            match self.workers[idx].query_task_length() {
-                Some(number_of_tasks) => {
-                    if number_of_tasks < minimum_workload {
-                        most_free_worker = idx;
-                        minimum_workload = number_of_tasks;
-                    }
-                }
-                None => {
-                    let tasks = self.workers[idx].queue_until_completion(task);
-                    return Message::SuccessfulDownloads(tasks);
-                }
-            }
-        }
-
-        let tasks = self.workers[most_free_worker].queue_until_completion(task);
-        Message::SuccessfulDownloads(tasks)
-    }
-
-    pub fn get_busy_worker_count(&self) -> usize {
-        self.workers.iter().map(|worker| {
-            if *worker.running.lock().unwrap() { 1 } else { 0 }
-        }).sum::<usize>()
+        println!("[WORKER] finished downloading {}", self.target.name);
     }
 }
 
