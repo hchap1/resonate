724d8ccad6301511559f64db4d62907bdf3a6c0c
hchap1 <hchap1@hotmail.com>
2025-03-08 14:44:27 +1100

    added yt-dlp to install script for windows

--- windows_dependencies.bat
+++ windows_dependencies.bat
@@ -1,10 +1,12 @@
 @echo off
 echo Installing Dependencies...
 winget install winget install --id Google.Chrome --exact --silent --accept-package-agreements --accept-source-agreements
-echo CHROME installed [1/3]
+echo CHROME installed [1/4]
 winget install Chromium.ChromeDriver
-echo CHROMEDRIVER installed [2/3]
+echo CHROMEDRIVER installed [2/4]
 winget install SQLite.SQlite
-echo SQLITE installed [3/3]
+echo SQLITE installed [3/4]
+winget install yt-dlp
+echo YTDLP installed [4/4]
 echo FINISHED
 pause
